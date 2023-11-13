46476e8da1370d70190e7dc9798d395564cb197a
GVassar <51205052+TheGareBear@users.noreply.github.com>
2023-11-13 11:46:20 -0700

    camera rotation minmax along X axis

--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -8,15 +8,35 @@ public class NewBehaviourScript : MonoBehaviour
     public float lookSensitivity;
     public float minXLook;
     public float maxXLook;
-    public Transform camanchor;
+    public Transform camAnchor;
     public bool invertXRotation;
     private float curXRot;
 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // called at the end of each frame
     void LateUpdate() 
     {
+        // get mouse x and y inputs
         float x = Input.GetAxis("Mouse X");
         float y = Input.GetAxis("Mouse Y");
 
+        // rotate player horizontally
         transform.eulerAngles += Vector3.up * x * lookSensitivity;
+        
+        // camera rotation vertically
+        if (invertXRotation)
+            curXRot += y * lookSensitivity;
+        else
+            curXRot -= y * lookSensitivity;
+
+        curXRot = Mathf.Clamp(curXRot, minXLook, maxXLook);
+
+        Vector3 clampedAngle = camAnchor.eulerAngles;
+        clampedAngle.x =curXRot;
+        camAnchor.eulerAngles = clampedAngle;
     }
 }
