fa49d24a95b22e2397749f404619273ba2f7cee4
GVassar <51205052+TheGareBear@users.noreply.github.com>
2023-11-13 11:32:47 -0700

    Camera rotation along y axis

--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float lookSensitivity;
+    public float minXLook;
+    public float maxXLook;
+    public Transform camanchor;
+    public bool invertXRotation;
+    private float curXRot;
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate() 
     {
-        
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles += Vector3.up * x * lookSensitivity;
     }
 }
